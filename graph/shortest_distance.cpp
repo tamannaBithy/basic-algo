b752104a6e8d6808a15de0853c85c5fd6fe9f1b9
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-02-05 02:08:24 +0800

    shortest_distance

---
+++ graph/shortest_distance.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1005];
+bool visited[1005];
+int level[1005];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0;
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        for (int child : adj_list[par])
+        {
+            if (!visited[child])
+            {
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[par] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    memset(level, -1, sizeof(level));
+    int src, dst;
+    cin >> src >> dst;
+    bfs(src);
+
+    cout << level[dst] << endl;
+
+    return 0;
+}
