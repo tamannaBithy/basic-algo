3f3311ec3df8df852433b18b8f60d13fa8e7c5ba
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-02-07 02:56:48 +0800

    dfs_traversal

--- graph/dfs/dfs_traversal.cpp
+++ graph/dfs/dfs_traversal.cpp
@@ -1,8 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> adj_list[1005];
+bool vis[1005];
+
+void dfs(int src)
+{
+    cout << src << " ";
+    vis[src] = true;
+    for (int child : adj_list[src])
+    {
+        if (!vis[child])
+            dfs(child);
+    }
+}
 
 int main()
 {
 
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    memset(vis, false, sizeof(vis));
+    dfs(0);
+
     return 0;
 }
+
+// 7 7
+// 0 2
+// 2 4
+// 0 1
+// 1 5
+// 1 3
+// 0 3
+// 3 6
