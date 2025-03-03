e700f5db1371b40917c55a20db76915486bf1d6f
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-03-03 15:28:46 +0600

    fibonacci

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int fibo(int n)
+{
+    if (n < 2)
+        return n;
+    return fibo(n - 1) + fibo(n - 2); // O(2^n)
+}
+
 int main()
 {
-
+    int n;
+    cin >> n;
+    cout << fibo(n);
     return 0;
 }
+
+/// ai recursioner time complexity O(2^n) ja khub baje. 40/45 er fibonacci ber korte gelei TLE ase. er theke boro numberer fibonacci konovabei ai recursion diye kora somvob na that's why we need to optimize it.
