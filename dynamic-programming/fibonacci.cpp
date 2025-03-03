2c82c22d96c1461e36c9abe991b4133138b31a9a
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-03-03 15:30:09 +0600

    fibonacci_TLE

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -16,4 +16,4 @@ int main()
     return 0;
 }
 
-/// ai recursioner time complexity O(2^n) ja khub baje. 40/45 er fibonacci ber korte gelei TLE ase. er theke boro numberer fibonacci konovabei ai recursion diye kora somvob na that's why we need to optimize it.
+/// ai recursioner time complexity O(2^n) ja khub baje. 40/45 er fibonacci ber korte gelei TLE ase & normally online judge gulate runtime 1s thake er besi hoile TLE hoy. er theke boro numberer fibonacci konovabei ai recursion diye kora somvob na that's why we need to optimize it.
