965d1c4f05cb307b7b2676b209fd441f098c954d
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-03-03 16:03:10 +0600

    fibonacci_with_memoization

--- dynamic-programming/fibonacci_with_memoization.cpp
+++ dynamic-programming/fibonacci_with_memoization.cpp
@@ -8,7 +8,7 @@ int fibo(long long int n)
         return n;
     if (dp[n] != -1)
         return dp[n];
-    dp[n] = fibo(n - 1) + fibo(n - 2);
+    dp[n] = fibo(n - 1) + fibo(n - 2); // this is the memoization
     return dp[n];
 }
 
